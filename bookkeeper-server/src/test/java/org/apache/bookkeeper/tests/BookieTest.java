183ea8dd1e581c906ee066373b1f3e3084f5f7e4
Gabriele <gabriele.quatrana@outlook.com>
2021-06-19 23:36:12 +0200

    Test

--- bookkeeper-server/src/test/java/org/apache/bookkeeper/tests/BookieTest.java
+++ bookkeeper-server/src/test/java/org/apache/bookkeeper/tests/BookieTest.java
@@ -7,11 +7,11 @@ import java.util.Arrays;
 import java.util.Collection;
 
 import org.apache.bookkeeper.bookie.Bookie;
+import org.apache.bookkeeper.bookie.Bookie.NoLedgerException;
 import org.apache.bookkeeper.bookie.BookieException;
 import org.apache.bookkeeper.bookie.BookieImpl;
 import org.apache.bookkeeper.conf.ServerConfiguration;
 import org.apache.bookkeeper.proto.BookkeeperInternalCallbacks.WriteCallback;
-
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -20,22 +20,23 @@ import org.junit.runners.Parameterized;
 import org.junit.runners.Parameterized.Parameters;
 
 import io.netty.buffer.ByteBuf;
+import io.netty.buffer.ByteBufAllocator;
 import io.netty.buffer.Unpooled;
 
 @RunWith(Parameterized.class)
 public class BookieTest {
-	
+
 	private Bookie bookie;
 	private ServerConfiguration conf;
-	
+
 	private static final int ENTRY_SIZE = 1024;
-	
+
 	private ByteBuf entry;
 	private boolean ackBeforeSync;
 	private WriteCallback cb;
 	private Object ctx;
 	private byte[] masterKey;
-	
+
 	public BookieTest(ByteBuf entry, boolean ackBeforeSync, WriteCallback cb, Object ctx, byte[] masterKey) {
 		this.entry = entry;
 		this.ackBeforeSync = ackBeforeSync;
@@ -43,47 +44,51 @@ public class BookieTest {
 		this.ctx = ctx;
 		this.masterKey = masterKey;
 	}
-	
+
 	@Parameters
 	public static Collection<Object[]> getParameters() {
 		return Arrays.asList(new Object[][] {
 			{ Unpooled.wrappedBuffer(new byte[ENTRY_SIZE]), false, null, null, new byte[512] },
-			{ Unpooled.wrappedBuffer(new byte[ENTRY_SIZE]), true, null, null, new byte[ENTRY_SIZE]},
+			{ Unpooled.wrappedBuffer(new byte[ENTRY_SIZE]), true, null, null, new byte[ENTRY_SIZE] },
 		});
 	}
-	
+
 	@Before
 	public void setUp() throws IOException, InterruptedException, BookieException {
 		conf = TestConfiguration.getConfiguration();
 		bookie = new BookieImpl(conf);
-		//bookie.start();
+		// bookie.start();
 	}
-	
+
 	@After
 	public void cleanUp() {
 		bookie.shutdown();
 		conf.clear();
 	}
-	
+	/*
 	@Test
-	public void test() throws IOException, BookieException, InterruptedException {
-		System.out.println("----------- ADD -----------");
-		System.out.println("Entry: " + entry);
-		System.out.println("Ack Before Sync: " + ackBeforeSync);
-		System.out.println("Write Callback: " + cb);
-		System.out.println("CTX: " + ctx);
-		System.out.println("Master Key: " + masterKey);
-		System.out.println("PRIMA: " + bookie.getTotalFreeSpace());
-		System.out.println("TOT: " + bookie.getTotalDiskSpace());
+	public void launch() throws IOException, BookieException, InterruptedException {
+		bookie.start();
+		System.out.println("A: " + bookie.isRunning());
+
+		bookie.recoveryAddEntry(entry, cb, ctx, masterKey);
 		
-		System.out.println("DIOPORCO");
-		
-		bookie.addEntry(entry, ackBeforeSync, cb, ctx, masterKey);
-		
-		System.out.println("DOPO: " + bookie.getTotalFreeSpace());
+		long entryId = entry.readerIndex();
+		long ledgerId = entry.getLong((int) entryId);
+
+		System.out.println("\n----------- READ -----------");
+		System.out.println("LedgerId: " + ledgerId);
+		System.out.println("EntryId: " + entryId);
+
+		ByteBuf actual = bookie.readEntry(ledgerId, entryId);
+
+		System.out.println("\n---------- RESULT ----------");
+		System.out.println("Expected: " + entry);
+		System.out.println("Actual: " + actual);
 		
 		assertEquals(2,2);
-	}
+	}*/
+
 	
 	@Test
 	public void testAddAndRead() throws IOException, BookieException, InterruptedException {
@@ -93,27 +98,25 @@ public class BookieTest {
 		System.out.println("Write Callback: " + cb);
 		System.out.println("CTX: " + ctx);
 		System.out.println("Master Key: " + masterKey);
-		
-		System.out.println("DIOPORCO");
-		
+
 		bookie.addEntry(entry, ackBeforeSync, cb, ctx, masterKey);
-		
+
 		long entryId = entry.readerIndex();
-		long ledgerId = entry.getLong((int)entryId);
-		
+		long ledgerId = entry.getLong((int) entryId);
+
 		System.out.println("\n----------- READ -----------");
 		System.out.println("LedgerId: " + ledgerId);
 		System.out.println("EntryId: " + entryId);
-		
+
 		ByteBuf actual = bookie.readEntry(ledgerId, entryId);
-		
+
 		System.out.println("\n---------- RESULT ----------");
 		System.out.println("Expected: " + entry);
 		System.out.println("Actual: " + actual);
-		
+
 		assertEquals(entry, actual);
 	}
-	
+
 	@Test
 	public void testRecoveryAddEntry() throws IOException, BookieException, InterruptedException {
 		System.out.println("----------- ADD -----------");
@@ -122,25 +125,62 @@ public class BookieTest {
 		System.out.println("Write Callback: " + cb);
 		System.out.println("CTX: " + ctx);
 		System.out.println("Master Key: " + masterKey);
-		
-		System.out.println("DIOPORCO");
-		
+
 		long entryId = entry.readerIndex();
-		long ledgerId = entry.getLong((int)entryId);
-		
-		bookie.fenceLedger(ledgerId, masterKey);	
+		long ledgerId = entry.getLong((int) entryId);
+
+		bookie.fenceLedger(ledgerId, masterKey);
 		bookie.recoveryAddEntry(entry, cb, ctx, masterKey);
-		
+
 		System.out.println("\n----------- READ -----------");
 		System.out.println("LedgerId: " + ledgerId);
 		System.out.println("EntryId: " + entryId);
-		
+
 		ByteBuf actual = bookie.readEntry(ledgerId, entryId);
-		
+
 		System.out.println("\n---------- RESULT ----------");
 		System.out.println("Expected: " + entry);
 		System.out.println("Actual: " + actual);
-		
+
 		assertEquals(entry, actual);
+	}
+
+	@Test
+	public void testSetAndGetExplicitLac() throws NoLedgerException, IOException, InterruptedException, BookieException {
+		System.out.println("----------- ADD -----------");
+		System.out.println("Entry: " + entry);
+		System.out.println("Ack Before Sync: " + ackBeforeSync);
+		System.out.println("Write Callback: " + cb);
+		System.out.println("CTX: " + ctx);
+		System.out.println("Master Key: " + masterKey);
+
+		long entryId = entry.readerIndex();
+		long ledgerId = entry.getLong((int) entryId);
+
+		bookie.setExplicitLac(entry, cb, ctx, masterKey);
+
+		System.out.println("\n----------- READ -----------");
+		System.out.println("LedgerId: " + ledgerId);
+		System.out.println("EntryId: " + entryId);
+
+		ByteBuf actual = bookie.getExplicitLac(ledgerId);
+
+		System.out.println("\n---------- RESULT ----------");
+		System.out.println("Expected: " + entry);
+		System.out.println("Actual: " + actual);
+
+		ByteBuf expected = createExplicitLACEntry(ledgerId, entry);
+
+		assertEquals(2, 2);
+	}
+
+	private ByteBuf createExplicitLACEntry(long ledgerId, ByteBuf explicitLac) {
+		ByteBufAllocator allocator = ByteBufAllocator.DEFAULT;
+		ByteBuf bb = allocator.directBuffer(8 + 8 + 4 + explicitLac.capacity());
+		bb.writeLong(ledgerId);
+		bb.writeLong(-0x8000);
+		bb.writeInt(explicitLac.capacity());
+		bb.writeBytes(explicitLac);
+		return bb;
 	}
 }
