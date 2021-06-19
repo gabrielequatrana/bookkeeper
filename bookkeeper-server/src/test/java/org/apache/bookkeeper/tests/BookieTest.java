fa003edccf813eb8010e77fff9c7c99d71f4c744
Gabriele <gabriele.quatrana@outlook.com>
2021-06-19 16:50:46 +0200

    Test #1

--- bookkeeper-server/src/test/java/org/apache/bookkeeper/tests/BookieTest.java
+++ bookkeeper-server/src/test/java/org/apache/bookkeeper/tests/BookieTest.java
@@ -55,6 +55,7 @@ public class BookieTest {
 	public void setUp() throws IOException, InterruptedException, BookieException {
 		conf = TestConfiguration.getConfiguration();
 		bookie = new BookieImpl(conf);
+		bookie.start();
 	}
 	
 	@After
