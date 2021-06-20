e77dd3b795b70f0f8c7a5b4c141fb0adc773e1fa
Gabriele <gabriele.quatrana@outlook.com>
2021-06-20 19:32:41 +0200

    Test #1

--- bookkeeper-server/src/test/java/org/apache/bookkeeper/tests/BookieTest.java
+++ bookkeeper-server/src/test/java/org/apache/bookkeeper/tests/BookieTest.java
@@ -72,12 +72,12 @@ public class BookieTest {
 			
 			// Minimal test suite
 			{ 1L, 1L, false, null, new byte[0], null },
-			{ 0L, 0L, false, null, new byte[0], null },
+			/*{ 0L, 0L, false, null, new byte[0], null },
 			{ 0L, 1L, false , "ledger-test", new byte[0], null},
 			{ -1L, -1L, false, new String(), new byte[0], IllegalArgumentException.class },
 			{ 2L, -1L, true, "ledger-test", new byte[1], IndexOutOfBoundsException.class},
 			{ 1L, 2L, true, new String(), null, NullPointerException.class}
-		});
+		*/});
 	}
 	
 	@Before
